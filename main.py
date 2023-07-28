ec546ef0f02c1477cfb7e7cc21f71347fee2cc41
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2023-07-28 20:18:49 +0330

    Start 🚀

---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def fetch_forex_factory_economic_calendar():
+    url = 'https://www.forexfactory.com/calendar.php'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+
+    print(f"{response.status_code} 🏁")
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        event_rows = soup.find_all('tr', class_='calendar__row')
+        calendar_data = []
+        # print(event_rows)
+        for row in event_rows:
+            try:
+                event_time = row.find(
+                    'td', class_='calendar__time').text.strip()
+                event_currency = row.find(
+                    'td', class_='calendar__currency').text.strip()
+                event_impact = row.find(
+                    'td', class_='calendar__impact').span['title']
+                event_description = row.find(
+                    'td', class_='calendar__event').text.strip()
+                event_actual = row.find(
+                    'td', class_='calendar__actual').text.strip()
+                event_forecast = row.find(
+                    'td', class_='calendar__forecast').text.strip()
+                event_previous = row.find(
+                    'td', class_='calendar__previous').text.strip()
+
+                calendar_data.append({
+                    'time': event_time,
+                    'currency': event_currency,
+                    'impact': event_impact,
+                    'description': event_description,
+                    'actual': event_actual,
+                    'forecast': event_forecast,
+                    'previous': event_previous,
+                })
+            except Exception as e:
+                # print('Error ❌ ' + str(e ) + ' ' + str(row))
+                pass
+
+        return calendar_data
+    else:
+        print("Failed to fetch data from Forex Factory.")
+        return None
+
+
+if __name__ == "__main__":
+    economic_calendar = fetch_forex_factory_economic_calendar()
+    if economic_calendar:
+        for idx, event in enumerate(economic_calendar, start=1):
+            print(f"Event {idx}:")
+            print(f"Time: {event['time']}")
+            print(f"Currency: {event['currency']}")
+            print(f"Impact: {event['impact']}")
+            print(f"Description: {event['description']}")
+            print(f"Actual: {event['actual']}")
+            print(f"Forecast: {event['forecast']}")
+            print(f"Previous: {event['previous']}")
+            print("----------------------")
+    else:
+        print("No economic calendar data available.")
+
